a62d1a6b49e9c68783fc64409acf170920f8fe91
Chukslord1 <Chukslord1@gmail.com>
2022-05-06 14:43:12 +0100

    fixed tests

--- Backend/VotingPortal/App/tests.py
+++ Backend/VotingPortal/App/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from . models import Time
 from django.test import TestCase
 from django.urls import reverse
 
@@ -9,9 +10,43 @@ class ViewTest(TestCase):
 
     def test_view_returns_OK(self):
         response = self.client.get('/')
+        response2 = self.client.get('/candidates.html')
+        response3 = self.client.get('/results.html')
+        response4 = self.client.get('/activity.html')
+        response5 = self.client.get('/login.html')
+        response6 = self.client.get('/register.html')
+        response7 = self.client.get('/candidate_reg')
+        response8 = self.client.get('/voters-approved')
+
         self.assertEqual(response.status_code,200)
+        self.assertEqual(response2.status_code,200)
+        self.assertEqual(response3.status_code,200)
+        self.assertEqual(response4.status_code,200)
+        self.assertEqual(response5.status_code,200)
+        self.assertEqual(response6.status_code,200)
+        self.assertEqual(response7.status_code,200)
+        self.assertEqual(response8.status_code,200)
 
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('APP:index'))
+        response2 = self.client.get(reverse('APP:create_vote'))
+        response3 = self.client.get(reverse('APP:results'))
+        response4 = self.client.get(reverse('APP:activity'))
+        response5 = self.client.get(reverse('APP:login'))
+        response6 = self.client.get(reverse('APP:register'))
+        response7 = self.client.get(reverse('APP:candidate_reg'))
+        response8 = self.client.get(reverse('APP:voters_approved'))
+
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index_new.html')
+        self.assertEqual(response3.status_code, 200)
+        self.assertTemplateUsed(response3, 'results.html')
+        self.assertEqual(response4.status_code, 200)
+        self.assertTemplateUsed(response4, 'activity_new.html')
+        self.assertEqual(response5.status_code, 200)
+        self.assertTemplateUsed(response5, 'login.html')
+        self.assertEqual(response5.status_code, 200)
+        self.assertTemplateUsed(response6, 'register.html')
+        self.assertEqual(response7.status_code, 200)
+        self.assertTemplateUsed(response7, 'candidate_reg.html')
+        self.assertEqual(response8.status_code, 200)
--- Backend/VotingPortal/App/views.py
+++ Backend/VotingPortal/App/views.py
@@ -34,56 +34,65 @@ def login(request):
     else:
         return render(request,"login.html")
 
-# the login view
+# helper function for create voter
+def create_voter(request):
+    if request.method=="POST":
+        last_name=request.POST.get('lastname')
+        name= request.POST.get('name')
+        name=name.replace(" ","")
+        phone=request.POST.get('phone')
+        sex=request.POST.get('sex')
+        dob=request.POST.get('dob')
+        admission_year=request.POST.get('admission_year')
+        graduation_year=request.POST.get('graduation_year')
+        chapter=request.POST.get('chapter')
+        chapter_year=request.POST.get('chapter_year')
+        attendance_status=request.POST.get("attendance_status")
+        dishonesty_status=request.POST.get("dishonesty")
+        username = name+last_name
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        # check for password confirmation
+        if password1 == password2:
+            # check if user exists
+            if UserProfile.objects.filter(phone=phone).exists() or User.objects.filter(username=username).exists():
+                context={"message":"user already exists"}
+                return render(request,"register.html",context)
+            else:
+                # create user with details collected
+                user = User.objects.create(username=username,first_name=name,password=password1, email=email)
+                #encrypt password
+                user.set_password(user.password)
+                user.save()
+                profile=UserProfile.objects.create(user=user,username=username,phone=phone,sex=sex,date_of_birth=dob,admission_year=admission_year,graduation_year=graduation_year,thsosa_chapter=chapter,attendance_status=attendance_status,dishonesty_status=dishonesty_status)
+                profile.save()
+                def_vote=Vote.objects.create(user=username,name="0000",title="0000")
+                def_vote.save()
+                return redirect("login.html")
+        else:
+            context={"message":"password dont match"}
+            return render(request,"register.html",context)
+            
+# the register view
 def register(request):
     # check if the voter's registration is within scheduled time
-    if datetime.datetime.now(timezone.utc)>Time.objects.get(name="Registration").start and datetime.datetime.now(timezone.utc)<Time.objects.get(name="Registration").end:
-        # collect voters information securely
-        if request.method=="POST":
-            last_name=request.POST.get('lastname')
-            name= request.POST.get('name')
-            name=name.replace(" ","")
-            phone=request.POST.get('phone')
-            sex=request.POST.get('sex')
-            dob=request.POST.get('dob')
-            admission_year=request.POST.get('admission_year')
-            graduation_year=request.POST.get('graduation_year')
-            chapter=request.POST.get('chapter')
-            chapter_year=request.POST.get('chapter_year')
-            attendance_status=request.POST.get("attendance_status")
-            dishonesty_status=request.POST.get("dishonesty")
-            username = name+last_name
-            email = request.POST.get("email")
-            password1 = request.POST.get("password1")
-            password2 = request.POST.get("password2")
-
-            # check for password confirmation
-            if password1 == password2:
-                # check if user exists
-                if UserProfile.objects.filter(phone=phone).exists() or User.objects.filter(username=username).exists():
-                    context={"message":"user already exists"}
-                    return render(request,"register.html",context)
-                else:
-                    # create user with details collected
-                    user = User.objects.create(username=username,first_name=name,password=password1, email=email)
-                    #encrypt password
-                    user.set_password(user.password)
-                    user.save()
-                    profile=UserProfile.objects.create(user=user,username=username,phone=phone,sex=sex,date_of_birth=dob,admission_year=admission_year,graduation_year=graduation_year,thsosa_chapter=chapter,attendance_status=attendance_status,dishonesty_status=dishonesty_status)
-                    profile.save()
-                    def_vote=Vote.objects.create(user=username,name="0000",title="0000")
-                    def_vote.save()
-                    return redirect("login.html")
-            else:
-                context={"message":"password dont match"}
-                return render(request,"register.html",context)
-        return render(request,"register.html")
-    elif datetime.datetime.now(timezone.utc)>Time.objects.get(name="Registration").end:
-        context={"message_2":"Registration Closed"}
-        return render(request,"reg_no.html",context)
+    if Time.objects.filter(name="Registration"):
+        if datetime.datetime.now(timezone.utc)>Time.objects.get(name="Registration").start and datetime.datetime.now(timezone.utc)<Time.objects.get(name="Registration").end:
+            # collect voters information securely
+            create_voter(request)
+            return render(request,"register.html")
+        elif datetime.datetime.now(timezone.utc)>Time.objects.get(name="Registration").end:
+            context={"message_2":"Registration Closed"}
+            return render(request,"reg_no.html",context)
+        else:
+            context={"message_3":"Registration Not Started"}
+            return render(request,"reg_no.html",context)
     else:
-        context={"message_3":"Registration Not Started"}
-        return render(request,"reg_no.html",context)
+        create_voter(request)
+        return render(request,"register.html")
+        
 
 # create vote views
 def create_vote(request):
@@ -152,50 +161,58 @@ def results(request):
     context={"elections":Election.objects.all(),"results":Candidate.objects.filter(approved=True)}
     return render(request,"results.html",context)
 
+# create candidate helper function
+def create_candidate(request):
+    if request.method=="POST":
+        title=request.POST.get("title")
+        lastname=request.POST.get("lastname")
+        othername=request.POST.get("othername")
+        address=request.POST.get("address")
+        sex=request.POST.get("sex")
+        date_of_birth=request.POST.get("dob")
+        occupation=request.POST.get("occupation")
+        email=request.POST.get("email")
+        phone=request.POST.get("phone")
+        education=request.POST.get("education")
+        chapter=request.POST.get("chapter")
+        chapter_year=request.POST.get("chapter_year")
+        profile=request.POST.get("profile")
+        executive_status=request.POST.get("executive")
+        admission_date=request.POST.get("admission_date")
+        graduation_date=request.POST.get("graduation_date")
+        executive_officer=request.POST.get("executive_officer")
+        financial=request.POST.get("financial")
+        attendance_status=request.POST.get("attendance_status")
+        dishonesty_status=request.POST.get("dishonesty")
+        position=request.POST.get("position")
+        image=request.FILES.get("image")
+        # check if candidate exists
+        if Candidate.objects.filter(title=position,name_title=title,profile=profile,name=lastname,other_name=othername, sex=sex, email=email):
+            context={"message":"A candiate with your info already exists"}
+        else:
+             # create a new candidate with the details provided
+            candidate_reg=Candidate.objects.create(title=position,name_title=title,profile=profile,name=lastname,other_name=othername,admission_date=admission_date,graduation_date=graduation_date,address=address,sex=sex,date_of_birth=date_of_birth,occupation=occupation,email=email,phone=phone,education=education,chapter=chapter,chapter_year=chapter_year,executive_status=executive_status,executive_officer=executive_officer,financial=financial,attendance_status=attendance_status,dishonesty_status=dishonesty_status,position=position,image=image)
+            candidate_reg.save()
+            context={"message":"Your Registration Is Successful...Please await an Email From Us in A Few Days Time"}
+        return render(request,"candidate_reg.html",context)
+
 # candidate registration view
 def candidate_reg(request):
     # check if registration is within scheduled time
-    if datetime.datetime.now(timezone.utc)>Time.objects.get(name="Registration").start and datetime.datetime.now(timezone.utc)<Time.objects.get(name="Registration").end:
-        # collect registration details
-        if request.method=="POST":
-            title=request.POST.get("title")
-            lastname=request.POST.get("lastname")
-            othername=request.POST.get("othername")
-            address=request.POST.get("address")
-            sex=request.POST.get("sex")
-            date_of_birth=request.POST.get("dob")
-            occupation=request.POST.get("occupation")
-            email=request.POST.get("email")
-            phone=request.POST.get("phone")
-            education=request.POST.get("education")
-            chapter=request.POST.get("chapter")
-            chapter_year=request.POST.get("chapter_year")
-            profile=request.POST.get("profile")
-            executive_status=request.POST.get("executive")
-            admission_date=request.POST.get("admission_date")
-            graduation_date=request.POST.get("graduation_date")
-            executive_officer=request.POST.get("executive_officer")
-            financial=request.POST.get("financial")
-            attendance_status=request.POST.get("attendance_status")
-            dishonesty_status=request.POST.get("dishonesty")
-            position=request.POST.get("position")
-            image=request.FILES.get("image")
-            # check if candidate exists
-            if Candidate.objects.filter(title=position,name_title=title,profile=profile,name=lastname,other_name=othername, sex=sex, email=email):
-                context={"message":"A candiate with your info already exists"}
-            else:
-                 # create a new candidate with the details provided
-                candidate_reg=Candidate.objects.create(title=position,name_title=title,profile=profile,name=lastname,other_name=othername,admission_date=admission_date,graduation_date=graduation_date,address=address,sex=sex,date_of_birth=date_of_birth,occupation=occupation,email=email,phone=phone,education=education,chapter=chapter,chapter_year=chapter_year,executive_status=executive_status,executive_officer=executive_officer,financial=financial,attendance_status=attendance_status,dishonesty_status=dishonesty_status,position=position,image=image)
-                candidate_reg.save()
-                context={"message":"Your Registration Is Successful...Please await an Email From Us in A Few Days Time"}
-            return render(request,"candidate_reg.html",context)
-        return render(request,"candidate_reg.html")
-    elif datetime.datetime.now(timezone.utc)>Time.objects.get(name="Registration").end:
-        context={"message_2":"Registration Has Closed"}
-        return render(request,"cand_reg_no.html",context)
+    if Time.objects.filter(name="Registration"):
+        if datetime.datetime.now(timezone.utc)>Time.objects.get(name="Registration").start and datetime.datetime.now(timezone.utc)<Time.objects.get(name="Registration").end:
+            # collect registration details
+            create_candidate(request)
+            return render(request,"candidate_reg.html")
+        elif datetime.datetime.now(timezone.utc)>Time.objects.get(name="Registration").end:
+            context={"message_2":"Registration Has Closed"}
+            return render(request,"cand_reg_no.html",context)
+        else:
+            context={"message_3":"Registration Has Not Yet Started"}
+            return render(request,"cand_reg_no.html",context)
     else:
-        context={"message_3":"Registration Has Not Yet Started"}
-        return render(request,"cand_reg_no.html",context)
+        create_candidate(request)
+        return render(request,"candidate_reg.html")
 
 # list of voters approved
 def voters_approved(request):
